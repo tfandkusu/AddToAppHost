a18ac7056f8238b4048ec4d34f6e5ffeb8b9dc22
Haruhiko Takada <haruhiko.takada@gmail.com>
2025-01-13 19:40:39 +0900

    Flutter Activity を呼べるようにした。

--- app/src/main/java/com/tfandkusu/addtoapphost/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/addtoapphost/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.tfandkusu.addtoapphost
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val startFlutter = findViewById<Button>(R.id.startFlutter)
+        startFlutter.setOnClickListener {
+            val intent = FlutterActivity.createDefaultIntent(this)
+            startActivity(intent)
+        }
     }
 }
