db3a8a011f085ac73583996d5e5e88f62ace535d
Haruhiko Takada <haruhiko.takada@omiai.inc>
2025-05-01 14:40:30 +0900

    FlutterEngineGroup を使った FlutterActivity 呼び出しまで作成。

--- app/src/main/java/com/tfandkusu/addtoapphost/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/addtoapphost/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.tfandkusu.addtoapphost
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import io.flutter.embedding.android.FlutterFragment
-import io.flutter.embedding.engine.FlutterEngineCache
-import io.flutter.plugin.common.MethodChannel
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private val observer: Observer<Boolean> = Observer { attach ->
-        if (attach) {
-            attachFlutterFragment()
-        } else {
-            detachFlutterFragment()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,30 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val startFlutter = findViewById<Button>(R.id.startFlutter)
         startFlutter.setOnClickListener {
-            val intent = Intent(this, FirstFlutterActivity::class.java)
-            startActivityForResult(intent, 0)
+            val intent = FlutterActivity.NewEngineInGroupIntentBuilder(
+                MyFlutterActivity::class.java,
+                MyApplication.FLUTTER_ENGINE_GROUP_ID
+            )
+                .build(this)
+            startActivity(intent)
         }
-        FlutterHandler.mainFlutterFragmentAttach.observeForever(observer)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        FlutterHandler.mainFlutterFragmentAttach.removeObserver(observer)
-    }
-
-    private fun attachFlutterFragment() {
-        Log.d("Takada", "attachFlutterFragment")
-        val flutterFragment1 = FirstFlutterFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.flutterFragment1, flutterFragment1)
-            .commitAllowingStateLoss()
-    }
-
-    private fun detachFlutterFragment() {
-        Log.d("Takada", "detachFlutterFragment")
-        val flutterFragment1 = supportFragmentManager.findFragmentById(R.id.flutterFragment1)!!
-        supportFragmentManager.beginTransaction()
-            .remove(flutterFragment1)
-            .commitAllowingStateLoss()
     }
 }
---
+++ app/src/main/java/com/tfandkusu/addtoapphost/MyFlutterActivity.kt
@@ -0,0 +1,7 @@
+package com.tfandkusu.addtoapphost
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MyFlutterActivity : FlutterActivity() {
+
+}
